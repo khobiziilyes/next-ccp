8b353bf31671659333fb76ae3ae0106d91cd202f
Ilyes Khobizi <khobizi.ilyes@gmail.com>
2024-11-25 23:08:05 +0100

    Integrated calcCcpRip logic.

--- src/lib/ccp/calc-ccp-rip.ts
+++ src/lib/ccp/calc-ccp-rip.ts
@@ -1,3 +1,16 @@
+import { EMPTY_MOD, MAGIC_NUM } from "@/consts";
+
+// This function is responsible for generating the CCP Rip, from the CCP number.
+// I've found the algorithm on an online forum, the code is written in VBA.
+// You can find more information about the algorithm here: https://www.mouwazaf-dz.com/t48131-topic
+
 export function calcCcpRip(ccp: string): string {
-  return "";
+  const ccpInt = Number.parseInt(ccp);
+  const multiplied = ccpInt * 100;
+  const mod = multiplied % MAGIC_NUM;
+
+  const ripInt = 97 - ((mod + EMPTY_MOD) % MAGIC_NUM);
+  const rip = String(ripInt).padStart(2, "0");
+
+  return rip;
 }
